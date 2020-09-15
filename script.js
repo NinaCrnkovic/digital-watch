058f4a7550aff13340b879287d08dae81ea7add1
Nina Crnković <nina.crnkovic.zg@gmail.com>
2020-09-15 19:34:32 +0200

    updated version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var curentH = new Date().getHours();
+/*var curentH = new Date().getHours();
 var curentM = new Date().getMinutes();
 var curentS = new Date().getSeconds();
 var hours = document.getElementById('hours');
@@ -7,26 +7,18 @@ var minutes = document.getElementById('minutes');
 var seconds = document.getElementById('seconds');
 hours.innerHTML = curentH;
 minutes.innerHTML = curentM;
-seconds.innerHTML = curentS;
+seconds.innerHTML = curentS;*/
 
+clock();
 function clock (){
-    curentS++
-    if (curentS === 60){
-        curentM ++;
-        curentS = 0;
-        if (curentM === 60){
-            curentH ++;
-            curentM = 0;
-            if (curentH === 24){
-                curentH = 0;
-            }
-        }
-
-    }
-
+    var curentH = new Date().getHours();
+    var curentM = new Date().getMinutes();
+    var curentS = new Date().getSeconds();
+    var hours = document.getElementById('hours');
+    var minutes = document.getElementById('minutes');
+    var seconds = document.getElementById('seconds');
     hours.innerHTML = (curentH < 10 ? "0" + curentH: curentH);
     minutes.innerHTML = (curentM < 10 ? "0" + curentM: curentM);
     seconds.innerHTML = (curentS < 10 ? "0" + curentS: curentS);
 }
-
 var interval = setInterval(clock, 1000);
